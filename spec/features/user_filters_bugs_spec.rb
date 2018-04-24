d199bdc066f81f8254a4ded94a94611ddc7a532f
danfrenette <dan.r.frenette@gmail.com>
2018-04-24 15:26:03 -0400

    Expand tests

--- spec/features/user_filters_bugs_spec.rb
+++ spec/features/user_filters_bugs_spec.rb
@@ -1,12 +1,39 @@
 require "spec_helper"
 
 feature "A user can filter bugs" do
-  specify "by country" do
-    visit root_path
+  context "on the home page" do
+    before(:each) do
+      visit root_path
+    end
 
-    select "US", from: "search_countries"
-    click_on "Search"
-    
-    expect(page).to have_content("{1=>114, 2=>99, 4=>125}")
+    specify "by country" do
+      select "US", from: "search_countries"
+      click_on "Search"
+
+      expect(page).to have_content("{1=>114, 2=>99, 4=>125}")
+    end
+
+    specify "by device" do
+      select "iPhone 5", from: "search_device_ids"
+      click_on "Search"
+
+      expect(page).to have_content("{6=>110, 1=>30, 3=>32, 8=>30}")
+    end
+
+    specify "by both" do
+      select "JP", from: "search_countries"
+      select "Droid DNA", from: "search_device_ids"
+      click_on "Search"
+
+      expect(page).to have_content("{7=>21}")
+    end
+
+    specify "by two countries" do
+      select "US", from: "search_countries"
+      select "JP", from: "search_countries"
+      click_on "Search"
+
+      expect(page).to have_content("{1=>114, 2=>99, 4=>125, 5=>109, 7=>117, 8=>116}")
+    end
   end
 end
