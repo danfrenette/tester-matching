44d4821458e3a7e4c8c0ba4953731e73adb14d8f
danfrenette <dan.r.frenette@gmail.com>
2018-04-24 14:40:43 -0400

    Load seed data into test database at the start of each test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424172737) do
+ActiveRecord::Schema.define(version: 2018_04_24_172737) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "bugs", force: :cascade do |t|
     t.integer "tester_id"
